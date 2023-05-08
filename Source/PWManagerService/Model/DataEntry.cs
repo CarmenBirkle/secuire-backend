a5aec9e319f43a8b20f98b0b78a4e860a0160501
s.bienhuels <s.bienhuels@hegla-hanic.de>
2023-05-08 21:29:15 +0200

    Model im Aufbau #52

---
+++ Source/PWManagerService/Model/DataEntry.cs
@@ -0,0 +1,11 @@
+﻿namespace PWManagerService.Model
+{
+    public abstract class DataEntry
+    {
+        public int Id { get; set; } 
+        public string Subject { get; set; }
+        public Dictionary<string, string> CustomTopics { get; set; }
+        public string Comment { get; set; }
+        public bool Favourite { get; set; }
+    }
+}
---
+++ Source/PWManagerService/Model/LoginEntry.cs
@@ -0,0 +1,9 @@
+﻿namespace PWManagerService.Model
+{
+    public class LoginEntry : DataEntry
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }    
+        public string Url { get; set; }
+    }
+}
